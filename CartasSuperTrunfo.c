add00fc4c57bd813823c167959e4f201751fa8f8
Luizleal01 <luiz.f.leal@outlook.com>
2025-03-06 19:31:55 +0000

    CARTA 1 SEM INTERAÇÃO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     char Carta01[4] = "A01";
     char Estado[10] = "Sao Paulo";
     char Cidade[10] = "Mauá";
-    float Populacao = 418161.0; 
+    float Populacao = 418161.0;
     int Ponto_turistico = 40;
-    float area_km2 = 61.937; 
+    float area_km2 = 61.937;
     float Pib = 15.287;
-    
 
     printf("Carta01: %s\n", Carta01);
-    printf("Estado: %s\n", Estado);
-    printf("Cidade: %s\n", Cidade);
-    printf("Populacao: %.0f\n", Populacao);
-    printf("Pontos turísticos: %d\n", Ponto_turistico);
-    printf("Área em km²: %f\n", area_km2);
-    printf("Pib: %f\n", Pib);
-    
+    scanf("%s", Carta01);
 
-    return 0; 
+    printf("Estado: %s\n", Estado); 
+    scanf("%s", Estado);
+
+    printf("Cidade: %s\n", Cidade);
+    scanf("%s", Cidade);
+
+    printf("Populacao: %.0f\n", Populacao);
+    scanf("%f", &Populacao);
+
+    printf("Pontos turísticos: %d\n", Ponto_turistico);
+    scanf("%d", &Ponto_turistico); 
+
+    printf("Área em km²: %.3f\n", area_km2);
+    scanf("%f", &area_km2); 
+
+    printf("Pib: %.3f\n", Pib);
+    scanf("%f", &Pib); 
+    
+    return 0;
 }
